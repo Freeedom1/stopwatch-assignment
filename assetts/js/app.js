4484d3b9bc258a28534b0eb11766068116553c4a
freeedom1 <jo4jamb@gmail.com>
2022-06-10 12:59:28 +0100

    stopwatch

---
+++ assetts/js/app.js
@@ -0,0 +1,6 @@
+console.log("nice")
+//begin stopwatch program
+const stopwatchDisplay = document.querySelector('.stopwatch-display')
+const stopwatchStartBtn = document.querySelector('btn.stopwatch-start')
+const stopwatchStopBtn = document.querySelector('btn.stopwatch-stop')
+const stopwatchREsetBtn = document.querySelector('btn.stopwatch-reset')
